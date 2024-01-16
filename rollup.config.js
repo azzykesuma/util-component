bebec93b37fe071b69b65c035022c36272c60414
Azzzy <official.azzy@gmail.com>
2024-01-16 15:36:08 +0700

    updating...

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ import del from 'rollup-plugin-delete';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const packageJson = require('./package.json');
 export default {
-  input: 'src/index.ts',
+  input: 'src/index.js',
   output: [
     {
       file: packageJson.main,
