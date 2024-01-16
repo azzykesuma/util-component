48b2daedc6b292e4b0f4305bfb17486edcc82f87
Azzzy <official.azzy@gmail.com>
2024-01-16 11:30:14 +0700

    initial comit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import TestComponent from './Components/TestComponent'
+import './App.css'
+
+function App() {
+
+  return (
+      <TestComponent />
+  )
+}
+
+export default App
